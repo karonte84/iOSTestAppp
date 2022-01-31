f5027bec9e4a9535427cc2ee06af3deaddf0acab
Michael Sanelli <michael.sanelli@accenture.com>
2022-01-31 09:28:54 +0100

    Initial Commit

---
+++ ProvaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProvaApp
+//
+//  Created by michael.sanelli on 31/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
